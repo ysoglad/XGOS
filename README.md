cb72071374262663243384e82e50ce8f30c177d1
ysoglad <fake@mail.com>
2021-02-12 16:53:47 +0100

    Init commit!

---
+++ README.md
@@ -0,0 +1,5 @@
+# Deploy
+
+Run with:
+`mvn compile exec:java`
+
---
+++ src/main/java/app/XgosRunner.java
@@ -0,0 +1,7 @@
+package app;
+
+public class XgosRunner {
+    public static void main(String[] args) {
+        System.out.println("Init!");
+    }
+}
