fbb6b393eb3ae94267ec98e0b0956c5099ba94b6
ysoglad <fake@mail.com>
2021-02-15 09:55:43 +0100

    Spring added

---
+++ src/main/java/com/app/XGOS/XgosApplication.java
@@ -0,0 +1,14 @@
+package com.app.XGOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XgosApplication {
+
+    public static void main(String[] args) {
+        System.out.println("lol");
+        SpringApplication.run(XgosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/XGOS/XgosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.XGOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XgosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
